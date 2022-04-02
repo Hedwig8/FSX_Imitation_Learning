ae1444a7ca00008a451d7d57f4a7196d92dcc587
Henrique Freitas <up201707046@fe.up.pt>
2022-04-02 18:07:57 +0100

    Add file architecture to separate training steps

---
+++ DataProcessing/dataConsidered.py
@@ -0,0 +1,12 @@
+
+
+def hammerhead_data():
+    pass
+
+def immelmann_data():
+    pass
+
+manoeuvre_data = {
+    'Hammerhead': hammerhead_data,
+    'Immelmann': immelmann_data,
+}
---
+++ DataProcessing/models.py
@@ -0,0 +1,14 @@
+
+
+
+def hammerhead_model():
+    pass
+
+def immelmann_model():
+    pass
+
+manoeuvre_model = {
+    'Hammerhead': hammerhead_model,
+    'Immelmann': immelmann_model,
+}
+
