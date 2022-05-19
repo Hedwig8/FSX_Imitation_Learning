4a293f7e9f6273e942234d322c5648910c8be415
Henrique Freitas <up201707046@fe.up.pt>
2022-05-19 17:57:38 +0100

    HalfCubanEight: self-starting execution; inputs providen aileron <-> elevator

--- ModelTraining/manoeuvres_data/halfCubanEight.py
+++ ModelTraining/manoeuvres_data/halfCubanEight.py
@@ -1,6 +1,6 @@
 
 def elevator_control(examples_list):
-    features = ['angle_of_attack', 'pitch', 'bank', 'velocity_world_y', 'velocity_body_z']
+    features = ['angle_of_attack', 'pitch', 'bank', 'velocity_world_y', 'velocity_body_z', 'aileron']
     new_listX = []
     new_listy = []
     for df in examples_list:
