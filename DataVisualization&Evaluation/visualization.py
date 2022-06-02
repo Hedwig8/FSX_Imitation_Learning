b8a89ac6d5abd259a3ca7304d0855aef37f704b1
Henrique Freitas <up201707046@fe.up.pt>
2022-06-02 20:48:12 +0100

    Fix some details

--- DataVisualization&Evaluation/visualization.py
+++ DataVisualization&Evaluation/visualization.py
@@ -1,14 +1,15 @@
 import glob
 import pandas as pd
+from math import isnan
 import plotly.express as px
 
 from visUtils import velocity_to_position, rotate_initial_heading
 from evaluation import manoeuvre_evaluation
 
-dataset_path = "../dataset"
+dataset_path = "../ProcessedDataset"
 id = "*"
 manoeuvre_quality = "Good"
-manoeuvre_name = "SteepCurve"
+manoeuvre_name = "Immelmann"
 
 examples_evals_list = []
 evals_list = []
@@ -17,6 +18,7 @@ for filename in glob.glob(f'{dataset_path}/{id}/{manoeuvre_quality}/{manoeuvre_n
     df = velocity_to_position(pd.read_csv(filename))
     df = rotate_initial_heading(df)
     eval, components = manoeuvre_evaluation[manoeuvre_name](df)
+    if eval < 60: continue
     examples_evals_list.append((df, eval))
     evals_list.append(eval)
     components_list.append(components)
@@ -31,6 +33,7 @@ max_eval = max(evals_list)
 trajetories_lines = px.line_3d()
 for example, eval in examples_evals_list:
     intensity_green_blue = 255 - (((eval - min_eval)/(max_eval-min_eval)) * 255)
+    intensity_green_blue = intensity_green_blue if not isnan(intensity_green_blue) else 0
     trajetories_lines.add_scatter3d(x=example['x'], y=example['y'], z=example['z'], marker=dict(
         size=1,
         sizemode='diameter',
