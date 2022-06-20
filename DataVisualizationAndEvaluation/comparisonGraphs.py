420c7efbe3d2bf825079ecdedcf4831c041d5062
Henrique Freitas <up201707046@fe.up.pt>
2022-06-20 17:34:38 +0100

    Add script for evaluation box graph

--- DataVisualizationAndEvaluation/comparisonGraphs.py
+++ DataVisualizationAndEvaluation/comparisonGraphs.py
@@ -11,37 +11,28 @@ from evaluation import manoeuvre_evaluation
 dataset_path = '../ProcessedDataset'
 id = '*'
 manoeuvre_quality = 'Good'
-manoeuvre_name = 'HalfCubanEight'
+manoeuvre_name = 'Split-S'
 
 results_path = '../Results'
 
-evals = []
+evals = {
+    'Total Eval': []
+}
 for filename in glob.glob(f'{dataset_path}/{id}/{manoeuvre_quality}/{manoeuvre_name}/*_1.csv', recursive=True):
     df = pd.read_csv(filename)
     eval, _ = manoeuvre_evaluation[manoeuvre_name](df)
-    evals.append(eval)
+    evals['Total Eval'].append(eval)
 
-results = []
-for filename in glob.glob(f'{results_path}/{id}/{manoeuvre_quality}/{manoeuvre_name}/*_1.csv', recursive=True):
-    df = pd.read_csv(filename)
-    eval, _ = manoeuvre_evaluation[manoeuvre_name](df)
-    results.append({'type': 'circle', 
-                    'xref': 'x',
-                   'yref': 'y',
-                   'x0': eval-1200,
-                   'y0': -0.005,
-                   'x1': eval+1200,
-                   'y1': .005,
-                   'fillcolor':'rgb(255, 64, 64)', 
-                   'line': {
-                       'width': 1.5,
-                   }})
-    print(eval)
+threshold = [1, .9, .75, .6]
+for t in threshold:
+    t_evals = []
+    for filename in glob.glob(f'{results_path}/{id}/{manoeuvre_quality}/{manoeuvre_name}/*_1.csv', recursive=True):
+        df = pd.read_csv(filename)
+        eval, _ = manoeuvre_evaluation[manoeuvre_name](df)
+        t_evals.append(eval)
+    evals[f'{t*100}%'] = t_evals
 
-#graph = ff.create_distplot([ evals ], ['Examples Evaluations'], curve_type='kde', bin_size=10000, show_hist=False, show_rug=False )
-#graph.update_layout(title_text=manoeuvre_name)
-
-violin = go.Box({'x':evals})
-lines = go.Layout(shapes=results, )
-figure = go.Figure(data=violin, layout=lines)
+figure = go.Figure()
+for key in evals.keys():
+    figure.add_trace( go.Box(x= evals[key], name=key))
 figure.show()
