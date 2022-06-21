d0eaa1261933271b381b1bae55ab031814ad369b
Henrique Freitas <up201707046@fe.up.pt>
2022-06-21 22:52:46 +0100

    Update results graphs

--- DataVisualizationAndEvaluation/comparisonGraphs.py
+++ DataVisualizationAndEvaluation/comparisonGraphs.py
@@ -1,8 +1,5 @@
 from glob import glob
-from turtle import color
 import pandas as pd
-import plotly.express as px
-import plotly.figure_factory as ff
 import plotly.graph_objects as go
 import glob
 
@@ -16,23 +13,30 @@ manoeuvre_name = 'Split-S'
 results_path = '../Results'
 
 evals = {
-    'Total Eval': []
+    'Examples': []
 }
 for filename in glob.glob(f'{dataset_path}/{id}/{manoeuvre_quality}/{manoeuvre_name}/*_1.csv', recursive=True):
     df = pd.read_csv(filename)
     eval, _ = manoeuvre_evaluation[manoeuvre_name](df)
-    evals['Total Eval'].append(eval)
+    evals['Examples'].append(eval)
 
 threshold = [1, .9, .75, .6]
 for t in threshold:
     t_evals = []
-    for filename in glob.glob(f'{results_path}/{id}/{manoeuvre_quality}/{manoeuvre_name}/*_1.csv', recursive=True):
+    for filename in glob.glob(f'{results_path}/{id}/{t}/{manoeuvre_name}/*_1.csv', recursive=True):
         df = pd.read_csv(filename)
         eval, _ = manoeuvre_evaluation[manoeuvre_name](df)
         t_evals.append(eval)
-    evals[f'{t*100}%'] = t_evals
+    evals[f'{int(t*100)}%'] = t_evals
 
 figure = go.Figure()
 for key in evals.keys():
     figure.add_trace( go.Box(x= evals[key], name=key))
+figure.update_layout(title=manoeuvre_name,
+                        xaxis=dict(tickfont=dict(size=25)),
+                        yaxis=dict(tickfont=dict(size=25)),
+                        boxgap=0,
+                        margin=dict(
+                            b=100
+                        ),)
 figure.show()
